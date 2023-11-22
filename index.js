947a6b00f8b0e611699ae1c61ea5bfe3d9fb0ee6
Nur-NobyRahaman <github943@gmail.com>
2023-11-22 10:54:22 +0600

    task 4 latter gread program

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let number = 1.33;
+// let number = 1.33;
 // console.log(number.toPrecision(1));
-let text = "bangladesh";
+// let text = "bangladesh";
 // console.log(text.charAt(2));
 //concat
 
-let text1 = "is beautiful country";
+// let text1 = "is beautiful country";
 // console.log(text.concat(text1));
 
 // slice
-let newText = text.slice(0, 2);
+// let newText = text.slice(0, 2);
 // console.log(newText);
 
 //  task 2
@@ -23,18 +23,57 @@ let newText = text.slice(0, 2);
 // console.log("full name 2nd position", fullName.slice(2, 3))
 
 // task 3
-let firstNumber = prompt("Enter First number : ");
-let lastNumber = prompt("Enter Last number : ");
-firstNumber = PaymentResponse(firstNumber);
-lastNumber = PaymentResponse(lastNumber);
-let sum = firstNumber + lastNumber;
-let sub = firstNumber - lastNumber;
-let mul = firstNumber * lastNumber;
-let div = firstNumber / lastNumber;
-let mod = firstNumber % lastNumber;
+// let firstNumber = prompt("Enter First number : ");
+// let lastNumber = prompt("Enter Last number : ");
+// firstNumber = PaymentResponse(firstNumber);
+// lastNumber = PaymentResponse(lastNumber);
+// let sum = firstNumber + lastNumber;
+// let sub = firstNumber - lastNumber;
+// let mul = firstNumber * lastNumber;
+// let div = firstNumber / lastNumber;
+// let mod = firstNumber % lastNumber;
 
-console.log(firstNumber, "+", lastNumber, "=", sum);
-console.log(firstNumber, "-", lastNumber, "=", sub);
-console.log(firstNumber, "*", lastNumber, "=", mul);
-console.log(firstNumber, "/", lastNumber, "=", div);
-console.log(firstNumber, "%", lastNumber, "=", mod);
+// console.log(firstNumber, "+", lastNumber, "=", sum);
+// console.log(firstNumber, "-", lastNumber, "=", sub);
+// console.log(firstNumber, "*", lastNumber, "=", mul);
+// console.log(firstNumber, "/", lastNumber, "=", div);
+// console.log(firstNumber, "%", lastNumber, "=", mod);
+
+// task 3
+
+let mark = parseInt(prompt("write your mark"));
+
+if (mark >= 80 && mark <= 100) {
+    console.log("A+")
+    document.write("A+")
+}
+else if (mark >= 70 && mark <= 79) {
+    console.log("A")
+    document.write("A")
+}
+else if (mark >= 60 && mark <= 69) {
+    console.log("A-")
+    document.write("A-")
+}
+else if (mark >= 50 && mark <= 59) {
+    console.log("B")
+    document.write("B")
+}
+else if (mark >= 40 && mark <= 49) {
+    console.log("C")
+    document.write("C")
+}
+else if (mark >= 33 && mark <= 39) {
+    console.log("D")
+    document.write("D")
+}
+else if (mark >= 0 && mark <= 32) {
+    console.log("Fail")
+    document.write("Fail")
+}
+    
+else {
+    document.write("write into 1-100")
+}
+
+
