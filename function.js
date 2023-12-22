940a680b9463f2ab0cad5c714d07e8b06dfab56b
Nur-NobyRahaman <github943@gmail.com>
2023-12-22 11:03:19 +0600

    function added

---
+++ function.js
@@ -0,0 +1,15 @@
+// var i = 5;
+// var result = i * i;
+// document.write(result + "<br></br>")
+
+// var i = 6;
+// var result = i * i;
+// document.write(result + "<b></b>")
+
+function square(k) {
+    return  k * k;
+  
+}
+
+const result = square(10);
+document.write(result + "<b></b>")  
